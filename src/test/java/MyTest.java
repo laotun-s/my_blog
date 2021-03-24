5106b60847cee8517c93f676f5b1e1a3ae91186e
laotun-s <752791819@qq.com>
2021-03-24 12:58:29 +0800

    设置首页文章显示数量

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,12 +1,4 @@
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import top.laotun.mapper.BlogUserMapper;
-import top.laotun.pojo.BlogUser;
-
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MyTest {
     @Test
